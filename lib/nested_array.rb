1ff9f850376594fea4318ea6bbbc3fde79e9ca7f
Alex Mata <alexmata92@ymail.com>
2020-08-03 20:12:16 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -62,7 +62,7 @@ def matrix_lookup(matrix, row, column)
   "Asparagus"].sort
     ]
     
-produce_room_storage[1][1]
+produce_room_storage[row][column]
 
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
